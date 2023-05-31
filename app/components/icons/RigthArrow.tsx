ad0fc854c459081bb8d089900ca45f843f422a0b
Mauro Sorgatto <mauro.sorgatto@azzero.tec.br>
2023-05-31 09:40:32 -0300

    add pasta com os icons

---
+++ app/components/icons/RigthArrow.tsx
@@ -0,0 +1,19 @@
+export const RigthArrow = () => {
+  return (
+    <svg
+      width="15"
+      height="14"
+      viewBox="0 0 15 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.66667 6.99996H13.3333M13.3333 6.99996L7.5 1.16663M13.3333 6.99996L7.5 12.8333"
+        stroke="white"
+        strokeWidth="1.66667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
